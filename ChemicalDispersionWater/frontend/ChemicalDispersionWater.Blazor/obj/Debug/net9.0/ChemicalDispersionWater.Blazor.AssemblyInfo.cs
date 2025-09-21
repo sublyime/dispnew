3a98a21c29977047ffc8f247e88be0d537d16a90
sublyime <sublyime@gmail.com>
2025-09-21 16:47:29 -0500

    zcvdx

--- ChemicalDispersionWater/frontend/ChemicalDispersionWater.Blazor/obj/Debug/net9.0/ChemicalDispersionWater.Blazor.AssemblyInfo.cs
+++ ChemicalDispersionWater/frontend/ChemicalDispersionWater.Blazor/obj/Debug/net9.0/ChemicalDispersionWater.Blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChemicalDispersionWater.Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba088a42df07ec7b2184c69c9811d16293fb6058")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d1dd0cb6db909e5b92576339c15d222d655de3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChemicalDispersionWater.Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChemicalDispersionWater.Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
